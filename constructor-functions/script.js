c5bb6a9eb0b61d7cba6375758b82b7500df5276f
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2023-06-04 22:47:58 -0300

    first commit

---
+++ constructor-functions/script.js
@@ -0,0 +1,30 @@
+// const Dom = {
+//   seletor: "li",
+//   element() {
+//     return document.querySelector(this.seletor);
+//   },
+//   ativar() {
+//     this.element().classList.add("ativar");
+//   },
+// };
+
+function Dom(seletor) {
+  const elementList = document.querySelectorAll(seletor);
+  this.elements = elementList;
+  this.addClass = function (classe) {
+    elementList.forEach((element) => {
+      element.classList.add(classe);
+    });
+  };
+  this.removeClass = function (classe) {
+    elementList.forEach((element) => {
+      element.classList.remove(classe);
+    });
+  };
+}
+
+const listaItens = new Dom("li");
+const ul = new Dom("ul");
+
+listaItens.addClass("ativar");
+ul.addClass("ul-ativo");
